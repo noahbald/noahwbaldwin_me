590be3422b38a394cf244bfd412b3f03f33eb622
Noah Baldwin <noahwbaldwin@protonmail.com>
2019-11-06 18:39:29 +1100

    Changed folder organisation

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import negIndex from '../../services/negIndex'
-import Button from '../button/button'
+import negIndex from '../services/negIndex'
+import Button from './button'
 
 import './gallery.css'
 
