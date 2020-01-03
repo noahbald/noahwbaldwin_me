e436a38cdb7557e7b14299a5717cf1213f6fac18
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-03 22:36:07 +1100

    Changes to Gallery appearance - Box shadows and negative margins to compensate - Arrow icons instead of rendered text - Fixed spacing between title and subtitle

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -4,6 +4,8 @@ import negIndex from '../services/negIndex'
 import Button from './button'
 
 import './gallery.css'
+import arrowLeft from './component-assets/arrow-left.svg'
+import arrowRight from './component-assets/arrow-right.svg'
 
 class Gallery extends React.Component {
   constructor(props) {
@@ -35,29 +37,27 @@ class Gallery extends React.Component {
     const visibleContent = this.getVisibleContent()
     return (
       <div className="gallery">
-        <Button text onClick={() => this.changePosition(-1)}>{'<'}</Button>
+        <Button type="text" icon={arrowLeft} onClick={() => this.changePosition(-1)} />
         {visibleContent.map((x, i) => {
           const posOverflow = Math.floor((position + i) / contents.length)
           const iPos = i - 1
-          let opacity = 1
+          const opacity = iPos < 0 || iPos > 1 ? 0 : 1
           const translateX = `translateX(calc(64px + (100% + 64px) * ${iPos}))`
-          if (iPos < 0 || iPos > 1) {
-            opacity = 0
-          }
+          
           return (
-            <div key={x.uid + posOverflow} className="gallery-content" style={{ transform: translateX, opacity }}>
-              <img className="gallery-image" style={{ backgroundImage: `url(${x.src})` }} alt="" />
+            <div key={x.uid + posOverflow} className="gallery-content soft-shadow" style={{ transform: translateX, opacity }}>
+              <div className="gallery-image" style={{ backgroundImage: `url(${x.src})` }} alt="" />
               <div className="card">
                 <h4>
                   <strong>{x.title}</strong>
-                  {' '}
+                  &nbsp;&mdash;&nbsp;
                   {x.subtitle}
                 </h4>
               </div>
             </div>
           )
         })}
-        <Button text onClick={() => this.changePosition(1)}>{'>'}</Button>
+        <Button type="text" icon={arrowRight} onClick={() => this.changePosition(1)} />
       </div>
     )
   }
