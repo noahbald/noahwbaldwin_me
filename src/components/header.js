a851cecc0dd631d4bcaa82f3a941f66d0f6f64ee
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-03 22:41:06 +1100

    Created reusable object for header - Moved title card to left of header

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './header.css'
+
+import Card from './card'
+
+export default function Header(props) {
+  const {
+    heading, src, title, subtitle,
+  } = props
+  return (
+    <section id="header" key={0}>
+      <h1 className="highlight-light-grey">{heading}</h1>
+      <div className="img-container soft-shadow" style={{ backgroundImage: `url("${src}")` }} />
+      <Card className="soft-shadow">
+        <h4>
+          <strong>{ title }</strong>
+          &nbsp;&mdash;&nbsp;
+          { subtitle }
+          <br />
+        </h4>
+      </Card>
+    </section>
+  )
+}
+
+Header.propTypes = {
+  heading: PropTypes.string,
+  src: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  heading: '',
+  src: 'https://via.placeholder.com/480x480?text=N/A',
+  title: '',
+  subtitle: '',
+}
