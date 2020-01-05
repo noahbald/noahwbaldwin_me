459b0c556abe8328321b4caaf9406b0d0cf75032
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-05 23:29:20 +1100

    More resume icons

--- src/pages/index/resumeFeatureBox.js
+++ src/pages/index/resumeFeatureBox.js
@@ -2,10 +2,13 @@ import uuid from '../../services/uid'
 
 import reactSVG from './resumeMedia/react.svg'
 import materialSVG from './resumeMedia/material-design.svg'
+import deloitteSVG from './resumeMedia/Deloitte.svg'
+import monashSVG from './resumeMedia/monash-logo-mono.svg'
+import schoolSVG from './resumeMedia/school.svg'
 
-const experience = `{"title":"Skills","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${reactSVG}","title":"React","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${materialSVG}","title":"User Experience & Design","body":"Let me create human focused experiences for your next project"}]}`
-const skills = `{"title":"Education","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${reactSVG}","title":"Software B.E at Monash","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${materialSVG}","title":"VCE at St Leonard's","body":"Let me create human focused experiences for your next project"}]}`
-const education = `{"title":"Experience","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${reactSVG}","title":"Deloitte Vacationer","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${materialSVG}","title":"Monash eSolutions","body":"Let me create human focused experiences for your next project"}]}`
+const experience = `{"title":"Experience","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${deloitteSVG}","title":"Deloitte Vacationer","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${monashSVG}","title":"Monash eSolutions","body":"Let me create human focused experiences for your next project"}]}`
+const skills = `{"title":"Skills","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${reactSVG}","title":"React","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${materialSVG}","title":"User Experience & Design","body":"Let me create human focused experiences for your next project"}]}`
+const education = `{"title":"Education","image":"https://picsum.photos/512/512","uid":"${uuid()}","contents":[{"uid":"${uuid()}","icon":"${monashSVG}","title":"Software B.E at Monash","body":"I can work with you and your next team's project in React, or quickly pick up your team's preferred framework."},{"uid":"${uuid()}","icon":"${schoolSVG}","title":"VCE at St Leonard's","body":"Let me create human focused experiences for your next project"}]}`
 const resumeContent = JSON.parse(`[${experience},${skills},${education}]`)
 
 export default resumeContent
