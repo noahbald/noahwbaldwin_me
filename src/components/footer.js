bd22954313a5fa4c2e8598178b92ef0fdabeb5d6
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-10 10:09:33 +1100

    Accessibility, responsiveness, optional return home button for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import Button from './button'
 
 import { ReactComponent as GitHub } from './component-assets/footer/github.svg'
 import { ReactComponent as LinkedIn } from './component-assets/footer/linkedin.svg'
@@ -7,41 +9,57 @@ import { ReactComponent as Instagram } from './component-assets/footer/instagram
 import { ReactComponent as Spotify } from './component-assets/footer/spotify.svg'
 import './footer.css'
 
-export default function Footer() {
+export default function Footer(props) {
+  const { homeButton } = props
   return (
     <footer key={3}>
+      {
+        homeButton ? (
+          <Button type="text" to="/">Return Home</Button>
+        ) : (
+          null
+        )
+      }
       <h2 className="highlight-primary">Say Hi,</h2>
       <h3 id="email"><a href="mailto:hi@noahwbaldwin.me">hi@noahwbaldwin.me</a></h3>
       <div className="footer-links">
-        <Link
-          to="https://github.com/noahbald"
+        <a
+          href="https://github.com/noahbald"
           style={{ display: 'inline-flex' }}
           title="Github"
         >
           <GitHub />
-        </Link>
-        <Link
-          to="https://www.linkedin.com/in/noahwbaldwin/"
+        </a>
+        <a
+          href="https://www.linkedin.com/in/noahwbaldwin/"
           style={{ display: 'inline-flex' }}
           title="LinkedIn"
         >
           <LinkedIn />
-        </Link>
-        <Link
-          to="https://www.instagram.com/noahbald/"
+        </a>
+        <a
+          href="https://www.instagram.com/noahbald/"
           style={{ display: 'inline-flex' }}
           title="Instagram"
         >
           <Instagram />
-        </Link>
-        <Link
-          to="https://www.instagram.com/noahbald/"
+        </a>
+        <a
+          href="https://www.instagram.com/noahbald/"
           style={{ display: 'inline-flex' }}
           title="Instagram"
         >
           <Spotify />
-        </Link>
+        </a>
       </div>
     </footer>
   )
+}
+
+Footer.propTypes = {
+  homeButton: PropTypes.bool,
+}
+
+Footer.defaultProps = {
+  homeButton: false,
 }
