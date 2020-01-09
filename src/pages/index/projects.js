b1cb3eb5234380fd295d976846d7f3382962c0a3
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-10 10:17:25 +1100

    index projects now use 'real' data, lazy loading

--- src/pages/index/projects.js
+++ src/pages/index/projects.js
@@ -13,46 +13,40 @@ import './projects.css'
 export default class Projects extends React.Component {
   constructor(props) {
     super(props)
-    this.galleryContents = [
-      {
-        uid: uuid(),
-        title: 'Lorem Ipsum Dolor',
-        subtitle: 'Lorem Ipsum Dolor Lorem Ipsum Dolor',
-        href: '#',
-        src: 'https://via.placeholder.com/480x480?text=one',
-      },
-      {
-        uid: uuid(),
-        title: 'Lorem Ipsum Dolor',
-        subtitle: 'Lorem Ipsum Dolor Lorem Ipsum Dolor',
-        href: '#',
-        src: 'https://via.placeholder.com/480x480?text=two',
-      },
-      {
-        uid: uuid(),
-        title: 'Lorem Ipsum Dolor',
-        subtitle: 'Lorem Ipsum Dolor Lorem Ipsum Dolor',
-        href: '#',
-        src: 'https://via.placeholder.com/480x480?text=three',
-      },
-      {
-        uid: uuid(),
-        title: 'Lorem Ipsum Dolor',
-        subtitle: 'Lorem Ipsum Dolor Lorem Ipsum Dolor',
-        href: '#',
-        src: 'https://via.placeholder.com/480x480?text=four',
-      },
-    ]
+    this.state = {
+      loading: true,
+      galleryContent: undefined,
+    }
+    this.loadContents()
+  }
+
+  async loadContents() {
+    try {
+      const galleryImport = await import('../../data/projects.json')
+      let galleryContent = galleryImport.default
+      galleryContent = galleryContent.filter(item => item.metadata.featured)
+      this.setState({
+        loading: false,
+        galleryContent
+      })
+    } catch (error) {
+      this.setState({
+        loading: true,
+      })
+    }
   }
 
   render() {
+    const { loading, galleryContent } = this.state
+    const { history } = this.props
+
     return (
       <section id="projects">
         <h2 className="highlight-secondary">Projects</h2>
-        <Button type="text">
-          <Link to=".">View More</Link>
+        <Button type="text" to="/projects">
+          View More
         </Button>
-        <Gallery contents={this.galleryContents} />
+        <Gallery contents={galleryContent} loading={loading} history={history} />
       </section>
     )
   }
