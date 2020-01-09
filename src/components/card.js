70c3858740bb5002541071c737a6f70131c7509e
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-10 10:07:51 +1100

    Added highlighting option for linkable cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from './link'
 
 import './card.css'
 
 export default function Card(props) {
-  const { className, children } = props
+  const { className, children, to } = props
+
+  let link = null
+  if (to || to === '') {
+    link = <Link to={to} tabIndex="-1" className="card-highlight" />
+  }
+
   return (
     <div className={`card ${className}`}>
       {children}
+      {link}
     </div>
   )
 }
@@ -15,9 +23,11 @@ export default function Card(props) {
 Card.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
+  to: PropTypes.string,
 }
 
 Card.defaultProps = {
   className: '',
   children: null,
+  to: undefined,
 }
