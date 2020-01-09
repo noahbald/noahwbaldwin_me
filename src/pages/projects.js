c1ad979f31179b827b8e960488bb19a593a8abad
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-01-10 10:00:34 +1100

    Created page for listing all projects

---
+++ src/pages/projects.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Footer from '../components/footer'
+import Card from '../components/card'
+import FeatureList, {FeatureListItem} from '../components/feature-list'
+
+import isProtocol from '../services/isProtocol'
+
+import './projects.css'
+
+function ProjectList(props) {
+  const { skeleton, data } = props
+
+  if (skeleton) {
+    return (
+      <section id="project-list">
+        <h2 className="highlight-secondary">
+          Projects
+        </h2>
+        <FeatureList skeleton />
+      </section>
+    )
+  }
+  if (!skeleton && !data) {
+    throw 'Failed to load project data'
+  }
+  return (
+    <section id="project-list">
+      <h2 className="highlight-secondary">
+        Projects
+      </h2>
+      <FeatureList>
+        {data.map((item) => {
+          return (
+            <FeatureListItem
+              key={item.uid}
+              src={item.src}
+              to={item.href}
+            >
+              <h4>
+                <strong>{item.title}</strong>
+              </h4>
+              <p>{item.subtitle}</p>
+              <p
+                style={{
+                  color: '#858898'
+                }}
+              >{item.type}</p>
+              <p
+                style={{
+                  position: 'absolute',
+                  bottom: 32,
+                  left: 32,
+                }}
+              >{item.metadata.year}</p>
+              <p
+                style = {{
+                  position: 'absolute',
+                  bottom: 32,
+                  right: 32,
+                }}
+              >{item.metadata.topics.join(', ')}</p>
+            </FeatureListItem>
+          )
+        })}
+      </FeatureList>
+    </section>
+  )
+}
+
+export default class Projects extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      data: undefined,
+    }
+    this.loadData()
+  }
+
+  async loadData() {
+    try {
+      const dataImport = await import('../data/projects.json')
+      this.setState({
+        loading: false,
+        data: dataImport.default,
+      })
+    } catch(e) {
+      this.setState({
+        loading: false,
+        data: null,
+      })
+    }
+  }
+
+  render() {
+    const { loading, data } = this.state
+    return (
+      <>
+        <ProjectList skeleton={loading} data={data} />
+        <Footer homeButton />
+      </>
+    )
+  }
+}
