0e4aaedc81655945e8e9bbc29cde006e1c866db3
Noah Baldwin <noahwbaldwin@protonmail.com>
2020-04-04 21:54:44 +1100

    Attempt to resolve routing issues on external hosts At the moment routes aren't working on wayback machine

--- src/App.js
+++ src/App.js
@@ -20,44 +20,64 @@ import Projects from './pages/projects'
 import ProjectPage from './pages/project-page'
 import Error404 from './pages/404'
 
+function MainRouter(props) {
+  const { location } = props
+
+  return (
+    <Route
+      location={location}
+      render={() => (
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="/projects/:href" component={ProjectPage} />
+          <Route path="/index*">
+            <Redirect to="/" />
+          </Route>
+          <Route component={Error404} status={404} />
+        </Switch>
+      )}
+    />
+  )
+}
+
 /**
  * Render an absolutely amazing website for an absolutely amazing human being
  */
 export default function App() {
+  const originalHost = /^http\w:\/\/w{3}?\.noahwbaldwin\.me/
+
+  // Return transitioning router if hosted as intended
+  if (window.location.href.match(originalHost)) {
+    return (
+      <Router forceRefresh={!('pushState' in window.history)}>
+        <Route
+          render={({ location }) => {
+            const { pathname } = location;
+            return (
+              <TransitionGroup>
+                <CSSTransition
+                  key={pathname}
+                  classNames="page"
+                  timeout={{
+                    enter: 1000,
+                    exit: 1000,
+                  }}
+                >
+                  <MainRouter location={location} />
+                </CSSTransition>
+              </TransitionGroup>
+            );
+          }}
+        />
+      </Router>
+    )
+  }
+
+  // Return normal router
   return (
-    <Router forceRefresh={!('pushState' in window.history)}>
-      <Route
-        render={({ location }) => {
-          const { pathname } = location;
-          return (
-            <TransitionGroup>
-              <CSSTransition
-                key={pathname}
-                classNames="page"
-                timeout={{
-                  enter: 1000,
-                  exit: 1000,
-                }}
-              >
-                <Route
-                  location={location}
-                  render={() => (
-                    <Switch>
-                      <Route exact path="/" component={Index} />
-                      <Route exact path="/projects" component={Projects} />
-                      <Route path="/projects/:href" component={ProjectPage} />
-                      <Route path="/index*">
-                        <Redirect to="/" />
-                      </Route>
-                      <Route component={Error404} status={404} />
-                    </Switch>
-                  )}
-                />
-              </CSSTransition>
-            </TransitionGroup>
-          );
-        }}
-      />
+    <Router>
+      <MainRouter />
     </Router>
   )
 }
